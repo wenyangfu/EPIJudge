87189f65c9d8f55819e6f1928b785d0e1af92e06
wenyangfu <wenyangfu@users.noreply.github.com>
2018-09-06 01:54:25 -0500

    Binary trees: reorder traversal. Implement recursive soln, blank out iterative to figure out myself

--- epi_judge_python/tree_preorder.py
+++ epi_judge_python/tree_preorder.py
@@ -2,20 +2,16 @@ from test_framework import generic_test
 
 
 def preorder_traversal(tree):
-    # TODO - you fill in here.
-    path, result = [tree], []
-    while path:
-        curr = path.pop()
-
+    # TODO: Iterative solution
 
 
     # Recursive solution
-    # result = []
-    # if tree:
-    #     result.append(tree.data)
-    #     result.extend(preorder_traversal(tree.left))
-    #     result.extend(preorder_traversal(tree.right))
-    # return result
+    result = []
+    if tree:
+        result.append(tree.data)
+        result.extend(preorder_traversal(tree.left))
+        result.extend(preorder_traversal(tree.right))
+    return result
 
 
 if __name__ == '__main__':
